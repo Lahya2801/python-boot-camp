d61fe5f90898c776dcd8e0e187d26ea7d9505f89
Lahya2801 <128461771+Lahya2801@users.noreply.github.com>
2024-07-19 15:33:43 +0530

    day1

---
+++ arithmeticop.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(f"sum of two numbers is {sum} \nsubtraction of two numbers is {sub}\n multiplication of 2 numbers is {mul}\n division of 2 numbers is {div} \nmodulus of 2 numbers is {a%b}")
