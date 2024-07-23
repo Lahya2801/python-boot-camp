6649676ea91e1c17f40d84a431ce38a61d70f597
Lahya2801 <128461771+Lahya2801@users.noreply.github.com>
2024-07-23 19:59:24 +0530

    day3

--- day3_prblm9.py
+++ day3_prblm9.py
@@ -6,10 +6,10 @@ leng=len(my_list)
 if(leng<k+N):
     print("ERROR")
 else: 
-    for i in range(len(my_list)):
+    for i in range(0,len(my_list)):
        print(my_list[k+N],end=" ")
        break        
-    #for i in range(leng):
+    #for i in range(leng):                                                           //we can do using these lines.
        #if(i==k+N): 
         #print("the element present in the (k+N)th position is:",my_list[i])  
 
